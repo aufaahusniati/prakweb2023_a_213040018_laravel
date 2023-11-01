6ba293d3dbee48a7503d230985e7994b36f84a5e
aufaahusniati <aufaahsnt21@gmail.com>
2023-11-01 15:39:36 +0700

    Video 7

--- applications/coba-laravel/app/Models/Post.php
+++ applications/coba-laravel/app/Models/Post.php
@@ -2,32 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Aufaa Husniati",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni necessitatibus quia adipisci temporibus! Dolorem, dolores quam placeat nulla consequuntur officiis modi aperiam quibusdam temporibus non possimus adipisci, expedita distinctio reprehenderit excepturi. Quo doloremque omnis nisi accusamus deserunt non autem repellat ad, ab, porro ducimus minus quasi adipisci alias officiis quaerat vitae eligendi ipsum quis cumque! Officiis, possimus praesentium? Beatae, facilis inventore magni maiores aliquid error obcaecati, repudiandae eius voluptatem harum doloribus magnam facere ratione nam nisi fuga officiis laboriosam nulla."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Upeh",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, repellat dignissimos et fugiat, voluptatibus dolor veritatis perferendis illum necessitatibus ea, reiciendis odio eius assumenda inventore! Aut natus architecto eos facere! Animi est possimus atque perspiciatis maiores voluptatum impedit voluptatem dolor, et eligendi iusto quod nam error alias cumque sit quidem velit nemo cum debitis. Dolores autem error quod delectus optio nulla voluptas voluptates corrupti deleniti! Hic sapiente a tempore asperiores maxime. Ipsam eveniet nam provident vero eos praesentium aperiam nobis sed ad placeat fugiat, voluptate, dolorem impedit harum reiciendis. Sint omnis dolorum commodi cumque velit voluptate, hic soluta quia dolor."
-        ],
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
+    // $fillable yang boleh diisi
+    // protected $fillable = ['title', 'excerpt', 'body'];
 
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // $guardedyang ga boleh di isi
+    protected $guarded = ['id'];
 }
--- applications/coba-laravel/resources/views/posts.blade.php
+++ applications/coba-laravel/resources/views/posts.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-    @foreach ($posts as $post)
-        <article class="mb-5">
-            <h2>
-                <a href="/posts/{{ $post["slug"] }}"> {{ $post["title"] }}</a>
-            </h2>
-            <h5>By: {{ $post["author"] }}</h5>
-            <p>{{ $post["body"] }}</p>
-        </article>
-    @endforeach
+@foreach ($posts as $post)
+<article class="mb-5">
+    <h2>
+        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        <p>{{ $post->excerpt }}</p>
+    </h2>
+    <p>{{ $post->excerpt }}</p>
+</article>
+@endforeach
 @endsection
-
