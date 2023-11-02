d702fbb02016849ed4a8a894702f2fab7e3ce499
aufaahusniati <aufaahsnt21@gmail.com>
2023-11-02 12:34:16 +0700

    Video 7

--- applications/coba-laravel/app/Models/Post.php
+++ applications/coba-laravel/app/Models/Post.php
@@ -14,4 +14,9 @@ class Post extends Model
 
     // $guardedyang ga boleh di isi
     protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- applications/coba-laravel/resources/views/post.blade.php
+++ applications/coba-laravel/resources/views/post.blade.php
@@ -4,6 +4,8 @@
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
 
+    <p>By. Aufaa Husniati in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
     <a href="/posts">Back to Post</a>
 </article>
