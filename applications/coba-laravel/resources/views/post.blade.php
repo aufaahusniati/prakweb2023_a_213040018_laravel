efcc68c6505b822c91bc57207e7f998d8a494ba6
aufaahusniati <aufaahsnt21@gmail.com>
2023-11-03 14:57:39 +0700

    Video 11

--- applications/coba-laravel/resources/views/post.blade.php
+++ applications/coba-laravel/resources/views/post.blade.php
@@ -2,12 +2,12 @@
 
 @section('container')
 <article>
-    <h1 class="mb-5">{{ $post->title }}</h1>
+    <h1 class="mb-5">{{ $post->title }}</h2>
 
-    <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-    {!! $post->body !!}
-    <a href="/posts" class="d-block mt-3">Back to Post</a>
+        {!! $post->body !!}
+        <a href="/posts" class="d-block mt-3">Back to Posts</a>
 </article>
 
 @endsection
