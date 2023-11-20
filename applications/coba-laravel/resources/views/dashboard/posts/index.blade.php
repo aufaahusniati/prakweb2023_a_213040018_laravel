6c6647b5b680562c8c8bc6ed4bf44ebae1342ddc
aufaahusniati <aufaahsnt21@gmail.com>
2023-11-20 14:05:40 +0700

    Video 20

--- applications/coba-laravel/resources/views/dashboard/posts/index.blade.php
+++ applications/coba-laravel/resources/views/dashboard/posts/index.blade.php
@@ -6,12 +6,12 @@
   </div>
 
   @if(session()->has('success'))
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success col-lg-8" role="alert">
       {{ session('success') }}
     </div>
   @endif
 
-  <div class="table-responsive col-lg">
+  <div class="table-responsive col-lg-8">
     <a href="/dashboard/posts/create" class="btn btn-primary mb-3"> Create new post</a>
         <table class="table table-striped table-sm">
           <thead>
@@ -31,7 +31,11 @@
               <td>
                 <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><i class="bi bi-eye"></i></a>
                 <a href="" class="badge bg-warning"><i class="bi bi-pencil-square"></i></a>
-                <a href="" class="badge bg-danger"><i class="bi bi-x-circle"></i></a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                  @method('delete')
+                  @csrf
+                    <button class="badge bg-danger border-0" onClick="return confirm('Are you sure?')"><i class="bi bi-x-circle"></i></button>
+                </form>
               </td>
             </tr>
             @endforeach
